7fc3f361d9a81dcc34793cc631f1a9f0cc467e39
Allan Queiroz <56939211+allanadfq@users.noreply.github.com>
2021-10-04 00:35:39 -0300

    added: the like/love function, updated the function that gets the token and the posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./posts.css";
 import api from "./../../services/api";
 import moment from "moment";
-import { BiLike } from "react-icons/bi";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FcLike } from "react-icons/fc";
 
 function Posts() {
   const [token, setToken] = React.useState("");
   const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
+    console.log("Entrou no 0000000");
     api
       .post(`/sign-in`, { username: "adfq", password: "421421" })
       .then((res) => setToken(res.data))
       .catch((error) => error.response);
-  });
+  }, []);
 
   React.useEffect(() => {
     if (token) {
+      console.log("Entrou no 123123123");
       api
         .get("/feeds", { headers: { Authorization: `Bearer ${token}` } })
         .then((response) => setData(response.data))
@@ -24,28 +28,74 @@ function Posts() {
     }
   }, [token]);
 
-  function handleLike() {
-    alert("liked");
+  function handleLike(id, type) {
+    if (token) {
+      if (type === "like") {
+        api
+          .post(
+            "/reaction",
+            {
+              feedId: id,
+              like: true,
+              // love: false,
+            },
+            { headers: { Authorization: `Bearer ${token}` } }
+          )
+          .then((res) => getFeeds())
+          .catch((err) => console.log(err));
+      } else {
+        api
+          .post(
+            "/reaction",
+            {
+              feedId: id,
+              love: true,
+              // like: false,
+            },
+            { headers: { Authorization: `Bearer ${token}` } }
+          )
+          .then((res) => getFeeds())
+          .catch((err) => console.log(err));
+      }
+    }
   }
+
+  function getFeeds() {
+    api
+      .get("/feeds", { headers: { Authorization: `Bearer ${token}` } })
+      .then((response) => setData(response.data))
+      .catch((error) => error.response);
+  }
+
   return (
     <div className="all-posts">
       <h2>Posts</h2>
       <hr />
       {data
         ? data.map(
-            ({ content, likes, id, createdAt, author: { username } }) => (
+            ({
+              id,
+              content,
+              likes,
+              loves,
+              createdAt,
+              author: { username },
+            }) => (
               <div className="a-post" key={id}>
                 <p>{content}</p>
-
                 <p>
                   {username} em{" "}
                   {moment(createdAt.slice(0, 10)).format("DD/MM/YYYY")}
                 </p>
                 <div style={{ display: "flex", alignItems: "center" }}>
-                  <button onClick={handleLike}>
-                    <BiLike size="1.2em" className="center" />
+                  <button onClick={() => handleLike(id, "like")}>
+                    <AiOutlineLike size="1.2em" className="center" />
                   </button>
                   <span>({likes})</span>
+                  <button onClick={() => handleLike(id, "love")}>
+                    <AiOutlineHeart size="1.2em" className="center" />
+                  </button>
+                  <span>({loves})</span>
                 </div>
               </div>
             )
